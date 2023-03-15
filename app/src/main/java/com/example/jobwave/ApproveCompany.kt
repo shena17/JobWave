015954ce302268990a54d280a7044590e1f1407b
Shehanx86 <it20274702@my.sliit.lk>
2023-03-15 14:56:34 +0530

    System admin

---
+++ app/src/main/java/com/example/jobwave/ApproveCompany.kt
@@ -0,0 +1,11 @@
+package com.example.jobwave
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ApproveCompany : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_approve_company)
+    }
+}
