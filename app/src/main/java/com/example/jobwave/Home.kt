34343d2ee089b6805dbb265b82b5cb3dc96c327d
Shenal <shenaldesilva17@gmail.com>
2023-05-04 12:05:17 +0530

    Login navigation made

--- app/src/main/java/com/example/jobwave/Home.kt
+++ app/src/main/java/com/example/jobwave/Home.kt
@@ -1,16 +1,21 @@
 package com.example.jobwave
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.appcompat.widget.AppCompatButton
+import com.google.firebase.auth.FirebaseAuth
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 class Home : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +25,6 @@ class Home : Fragment() {
      return inflater.inflate(R.layout.fragment_home, container, false)
 
     }
+
 
 }
--- app/src/main/java/com/example/jobwave/Login.kt
+++ app/src/main/java/com/example/jobwave/Login.kt
@@ -9,10 +9,14 @@ import android.widget.Toast
 import androidx.constraintlayout.helper.widget.MotionEffect.TAG
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private lateinit var databaseReference: DatabaseReference
+    private lateinit var uid: String
+    private lateinit var user: Users
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,12 +52,52 @@ class Login : AppCompatActivity() {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    Log.d(TAG, "signInWithEmail:success")
-                    // Sign in success, update UI with the signed-in user's information
-                    val intent = Intent(this, Dashboard::class.java)
-                    startActivity(intent)
-                    finish()
-                    Toast.makeText(baseContext, "Logged In", Toast.LENGTH_SHORT).show()
+                    auth = FirebaseAuth.getInstance()
+                    uid = auth.currentUser?.uid.toString()
+
+                    databaseReference = FirebaseDatabase.getInstance().getReference().child("Users")
+                    databaseReference.addListenerForSingleValueEvent(object :ValueEventListener{
+
+                        override fun onDataChange(snapshot: DataSnapshot) {
+
+                            if(snapshot.hasChild(uid)){
+
+                                databaseReference.child(uid).addValueEventListener(object : ValueEventListener{
+                                    override fun onDataChange(snapshot: DataSnapshot) {
+                                        user = snapshot.getValue(Users::class.java)!!
+
+                                        if(user.role == "Admin"){
+                                            val intent = Intent(this@Login, AdminMenu::class.java)
+                                            startActivity(intent)
+                                            Toast.makeText(baseContext, "Admin Logged In", Toast.LENGTH_SHORT).show()
+                                        }else if(user.role == "User"){
+                                            val intent = Intent(this@Login, Dashboard::class.java)
+                                            startActivity(intent)
+                                            Toast.makeText(baseContext, "User Logged In", Toast.LENGTH_SHORT).show()
+                                        }
+                                    }
+
+                                    override fun onCancelled(error: DatabaseError) {
+                                        TODO("Not yet implemented")
+                                    }
+
+                                })
+
+
+                            }else{
+                                val intent = Intent(this@Login, EmployerDashboard::class.java)
+                                startActivity(intent)
+                                Toast.makeText(baseContext, "Company Logged In", Toast.LENGTH_SHORT).show()
+                            }
+
+                        }
+
+                        override fun onCancelled(error: DatabaseError) {
+                            Toast.makeText(baseContext, "Cancelled!", Toast.LENGTH_SHORT).show()
+
+                        }
+
+                    })
                 } else {
                     Log.w(TAG, "signInWithEmail:failure", task.exception)
                     // If sign in fails, display a message to the user.
@@ -73,10 +117,53 @@ class Login : AppCompatActivity() {
         if(auth.currentUser == null){
             Toast.makeText(this, "Login to Continue", Toast.LENGTH_SHORT).show()
         }else{
-            val homeIntent = Intent(this, Dashboard::class.java)
-            startActivity(homeIntent)
-            finish()
-            Toast.makeText(baseContext, "Logged In", Toast.LENGTH_SHORT).show()
+
+            auth = FirebaseAuth.getInstance()
+            uid = auth.currentUser?.uid.toString()
+
+            databaseReference = FirebaseDatabase.getInstance().getReference().child("Users")
+            databaseReference.addListenerForSingleValueEvent(object :ValueEventListener{
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+
+                    if(snapshot.hasChild(uid)){
+
+                        databaseReference.child(uid).addValueEventListener(object : ValueEventListener{
+                            override fun onDataChange(snapshot: DataSnapshot) {
+                                user = snapshot.getValue(Users::class.java)!!
+
+                                if(user.role == "Admin"){
+                                    val intent = Intent(this@Login, AdminMenu::class.java)
+                                    startActivity(intent)
+                                    Toast.makeText(baseContext, "Admin Logged In", Toast.LENGTH_SHORT).show()
+                                }else if(user.role == "User"){
+                                    val intent = Intent(this@Login, Dashboard::class.java)
+                                    startActivity(intent)
+                                    Toast.makeText(baseContext, "User Logged In", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+
+                            override fun onCancelled(error: DatabaseError) {
+                                TODO("Not yet implemented")
+                            }
+
+                        })
+
+
+                    }else{
+                        val intent = Intent(this@Login, EmployerDashboard::class.java)
+                        startActivity(intent)
+                        Toast.makeText(baseContext, "Company Logged In", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(baseContext, "Cancelled!", Toast.LENGTH_SHORT).show()
+
+                }
+
+            })
         }
     }
 }
