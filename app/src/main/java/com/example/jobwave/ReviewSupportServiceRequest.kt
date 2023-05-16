24e60ad25493a47895ee7e017db197746eb1b47b
Rathnayake0 <gihanirathnayake39421@gmail.com>
2023-05-16 07:28:18 +0530

    Gihani Review page modified

--- app/src/main/java/com/example/jobwave/ReviewSupportServiceRequest.kt
+++ app/src/main/java/com/example/jobwave/ReviewSupportServiceRequest.kt
@@ -49,6 +49,7 @@ class ReviewSupportServiceRequest : AppCompatActivity() {
     }
 
     private fun deleteRecord(
+
         id: String
     ) {
         val dbRef = FirebaseDatabase.getInstance().getReference("Requests").child(id)
