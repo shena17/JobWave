ca68120b76c3b0ed97b01047e48e094d44a5339b
Rathnayake0 <gihanirathnayake39421@gmail.com>
2023-05-16 07:27:42 +0530

    Gihani Review page modified

--- app/src/main/java/com/example/jobwave/ReviewSupportServiceRequest.kt
+++ app/src/main/java/com/example/jobwave/ReviewSupportServiceRequest.kt
@@ -38,6 +38,7 @@ class ReviewSupportServiceRequest : AppCompatActivity() {
         }
 
         deletebtn.setOnClickListener {
+
             deleteRecord(
                 intent.getStringExtra("cusId").toString()
             )
