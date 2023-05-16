657ef4c69acd8586579903dc78371cc99b30dc7b
Rathnayake0 <gihanirathnayake39421@gmail.com>
2023-05-16 07:25:02 +0530

    Gihani Review page modified

--- app/src/main/java/com/example/jobwave/ReviewSupportServiceRequest.kt
+++ app/src/main/java/com/example/jobwave/ReviewSupportServiceRequest.kt
@@ -12,26 +12,17 @@ import com.google.firebase.database.FirebaseDatabase
 
 class ReviewSupportServiceRequest : AppCompatActivity() {
 
-    private lateinit var tvName: TextView
-
+    private lateinit var tvName : TextView
     private lateinit var tvEmail: TextView
-
     private lateinit var tvConcern: TextView
-
     private lateinit var editbtn: Button
-
     private lateinit var deletebtn: Button
 
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review_support_service_request)
 
         initView()
-
         setValuesToViews()
 
         editbtn.setOnClickListener {
@@ -40,7 +31,6 @@ class ReviewSupportServiceRequest : AppCompatActivity() {
                 intent.getStringExtra("name_f").toString()
             )
         }
-
 
         deletebtn.setOnClickListener {
             deleteRecord(
@@ -51,30 +41,31 @@ class ReviewSupportServiceRequest : AppCompatActivity() {
 
     }
 
-    private fun deleteRecord( id: String ) {
+    private fun deleteRecord(
+        id: String
+    ) {
         val dbRef = FirebaseDatabase.getInstance().getReference("Requests").child(id)
         val mTask = dbRef.removeValue()
 
         mTask.addOnSuccessListener {
-
             Toast.makeText(this, "Data record deleted", Toast.LENGTH_SHORT).show()
             val intent = Intent(this, SupportServiceRequest::class.java)
             finish()
             startActivity(intent)
-
         }.addOnFailureListener{ error ->
             Toast.makeText(this, "Error while deleting ${error.message}", Toast.LENGTH_SHORT).show()
         }
     }
 
-    private fun openUpdateDialog( cusId: String, name_f: String ) {
-
+    private fun openUpdateDialog(
+        cusId: String,
+        name_f: String
+    ) {
         val mDialog = AlertDialog.Builder(this)
         val inflater = layoutInflater
         val mDialogView = inflater.inflate(R.layout.editrequests, null)
 
         mDialog.setView(mDialogView)
-
 
         val etName = mDialogView.findViewById<EditText>(R.id.etName)
         val etEmail = mDialogView.findViewById<EditText>(R.id.etEmail)
@@ -87,13 +78,10 @@ class ReviewSupportServiceRequest : AppCompatActivity() {
 
         mDialog.setTitle("Updating $name_f Record")
 
-
         val alertDialog = mDialog.create()
-
         alertDialog.show()
 
         btnEditData.setOnClickListener {
-
             updateRequestData(
                 cusId,
                 etName.text.toString(),
@@ -101,12 +89,10 @@ class ReviewSupportServiceRequest : AppCompatActivity() {
                 etConcern.text.toString()
             )
 
-            //Toast message about the successful updating of data
-
+            //Toast message about the successful updating of dara
             Toast.makeText(applicationContext, "Data Updated Successfully", Toast.LENGTH_LONG).show()
 
             //Setting updated data to textviews
-
             tvName.text = etName.text.toString()
             tvEmail.text = etEmail.text.toString()
             tvConcern.text = etConcern.text.toString()
@@ -115,33 +101,31 @@ class ReviewSupportServiceRequest : AppCompatActivity() {
         }
     }
 
-    private fun updateRequestData( id:String, name:String, email:String, concern:String ) {
-
+    private fun updateRequestData(
+        id:String,
+        name:String,
+        email:String,
+        concern:String
+    ) {
         val dbRef = FirebaseDatabase.getInstance().getReference("Requests").child(id)
         val reqInfo = SupportServicesModel(id, name, email, concern)
-
         dbRef.setValue(reqInfo)
     }
 
     private fun setValuesToViews() {
-
+        TODO("Not yet implemented")
     }
 
     private fun initView() {
-
-        //Initializing the views
-
         tvName.text = intent.getStringExtra("name_f")
         tvEmail.text = intent.getStringExtra("email_f")
         tvConcern.text = intent.getStringExtra("concern_f")
     }
 
     override fun onBackPressed() {
-
         val setIntent = Intent(Intent.ACTION_MAIN)
         setIntent.addCategory(Intent.CATEGORY_HOME)
         setIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-
         startActivity(setIntent)
     }
 }
