92666946420af64d0b0c9150f93818523bfdd8a8
Rathnayake0 <gihanirathnayake39421@gmail.com>
2023-05-16 07:19:08 +0530

    commited

--- app/src/main/java/com/example/jobwave/ReviewSupportServiceRequest.kt
+++ app/src/main/java/com/example/jobwave/ReviewSupportServiceRequest.kt
@@ -18,6 +18,8 @@ class ReviewSupportServiceRequest : AppCompatActivity() {
     private lateinit var editbtn: Button
     private lateinit var deletebtn: Button
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review_support_service_request)
